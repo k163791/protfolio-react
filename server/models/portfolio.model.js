3931e0149894a4a213c91ba89d3605f4d8a00781
k163791 <k163791@nu.edu.pk>
2020-09-20 19:39:55 +0500

    fixed minor bug in route and model

--- server/models/portfolio.model.js
+++ server/models/portfolio.model.js
@@ -1,32 +1,31 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
 
 const PortfolioSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: 'Name is required'
-	},
-	email: {
-		type: String,
-		trim: true,
-		unique: 'Email already exists',
-		match: [/.+\@.+\..+/, "Please fill a valid email"]
-		required: 'Email is required'
-	},
-	created: {
-		type: Date,
-		default: Date.now(),
-	},
-	update: Date,
-	Github: String,
-	LinkedIn: String,
-	Projects: {
-		name: String,
-		technologies: [{ name: String }]
-		description: String,
-		date: Date
-	}
-})
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: "Email already exists",
+    match: [/.+\@.+\..+/, "Please fill a valid email"],
+    required: "Email is required",
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
+  update: Date,
+  Github: String,
+  LinkedIn: String,
+  Projects: {
+    name: String,
+    technologies: [],
+    description: String,
+    date: Date,
+  },
+});
 
 export default mongoose.model("Portfolio", PortfolioSchema);
--- server/routes/portfolio.routes.js
+++ server/routes/portfolio.routes.js
@@ -1,14 +1,18 @@
-import express from 'express'
-import PortfolioCtrl from '../controllers/portfolio.controller'
+import express from "express";
+import PortfolioCtrl from "../controllers/portfolio.controller";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("api/portfolio").get(portfolioCtrl.list).post(portfolioCtrl.create)
+router
+  .route("/api/portfolio")
+  .get(PortfolioCtrl.list)
+  .post(PortfolioCtrl.create);
 
-router.route("api/portfolio/:portfolioId")
-	.get(portfolioCtrl.read)
-	.post(portfolioCtrl.update)
+router
+  .route("/api/portfolio/:portfolioId")
+  .get(PortfolioCtrl.read)
+  .post(PortfolioCtrl.update);
 
-router.param("portfolioId", portfolioCtrl.portfolioById)
+router.param("portfolioId", PortfolioCtrl.portfolioById);
 
 export default router;
