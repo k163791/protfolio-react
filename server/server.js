d7f46351fb07ceda09256054be4dcf6eb80647e2
k163791 <k163791@nu.edu.pk>
2020-09-20 16:20:56 +0500

    initial server file

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import config from './../config/config'
+import app from './express'
+import mongoose from 'mongoose'
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/portfolio', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', () => {
+	throw new Error(`unable to connect to db: ${mongoUri}`);
+});
+
+app.listen(config.port, (err) => {
+	if(err) {
+		console.log(err)
+	}
+	console.log("Server started on port %s.", config.poty);
+}
