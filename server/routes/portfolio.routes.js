f1973d0799f05fef665bc60e4c318d2c304d5bab
k163791 <k163791@nu.edu.pk>
2020-09-20 17:12:18 +0500

    added routes for backend api

---
+++ server/routes/portfolio.routes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import PortfolioCtrl from '../controllers/portfolio.controller'
+
+const router = express.Router()
+
+router.route("api/portfolio").get(portfolioCtrl.list)
+
+router.route("api/portfolio/:portfolioId")
+	.get(portfolioCtrl.read)
+	.post(portfolioCtrl.update)
+
+router.param("portfolioId", portfolioCtrl.portfolioById)
+
+export default router;
