e63f8397c723dc38d5086bde5d57ffee200e2bd7
k163791 <k163791@nu.edu.pk>
2020-09-21 03:17:12 +0500

    added main router which contains all of our routes that are to be rendered

---
+++ client/MainRouter.js
@@ -0,0 +1,12 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "./core/Home";
+
+const MainRouter = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
+};
