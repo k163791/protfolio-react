ca6dc3f6642bbd8429ff7de92a20b2e1c8556323
k163791 <k163791@nu.edu.pk>
2020-09-21 14:56:15 +0500

    configured main router for navbar

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Portfolio from "./portfolio/Portfolio";
+import NavbarMenu from "./core/Navbar";
 
 const MainRouter = () => {
   return (
     <div>
+      <NavbarMenu />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/portfolio" component={Portfolio} />
       </Switch>
     </div>
   );
