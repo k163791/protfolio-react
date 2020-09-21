fb2a11516c96b5b4d87025495ba90a2667c4eb9d
k163791 <k163791@nu.edu.pk>
2020-09-21 14:57:27 +0500

    imported separate components into Home to render in one page

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,11 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Jumbotron, Button, Image } from "react-bootstrap";
+import backgr from "./../assets/images/background2.jpg";
+import Avatar from "./../assets/images/avatar.png";
+import Tech from "./Technologies";
+import About from "./AboutMe";
+import Projects from "./Projects";
 
 export default function Home() {
   return (
-    <div>
-      <Link to="/portfolio">Portfolio</Link>
-      <h1>Hello Friend</h1>
+    <div id="home">
+      <Jumbotron
+        fluid
+        style={{
+          backgroundImage: `url(${backgr})`,
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            color: "#fff",
+            fontWeight: "bold",
+          }}
+        >
+          <Image
+            src={Avatar}
+            roundedCircle
+            style={{ height: "171px", width: "180px" }}
+          />
+          <h1>Uzair Hussain</h1>
+          <p>Full Stack Engineer</p>
+          <p>
+            <Button variant="success">Edit Portfolio</Button>
+          </p>
+        </div>
+      </Jumbotron>
+      <About />
+      <Tech />
+      <Projects />
     </div>
   );
 }
