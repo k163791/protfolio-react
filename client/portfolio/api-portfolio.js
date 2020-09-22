78dd5e88a8ca4a77af08646e3dd2f8865702351e
k163791 <k163791@nu.edu.pk>
2020-09-22 12:24:29 +0500

    made api file for all the portfolio requests to the server

---
+++ client/portfolio/api-portfolio.js
@@ -0,0 +1,61 @@
+const create = async (user) => {
+  try {
+    let response = await fetch("/api/portfolio/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const list = async (signal) => {
+  try {
+    let response = await fetch("/api/portfolio/", {
+      method: "GET",
+      signal: signal,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const read = async (params, credentials, signal) => {
+  try {
+    let response = await fetch("/api/portfolio/" + params.portfolioId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const update = async (params, credentials, user) => {
+  try {
+    let response = await fetch("/api/portfolio/" + params.portfolioId, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { create, list, read, update };
